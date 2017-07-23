7d0a71b9131357d7f7d76b9865064b083fd8cf3b
theseanathan <seandarryanto@gmail.com>
2017-07-22 20:03:37 -0400

    Added twitterApi and working on redditApiWrapper
    Added twitter api functionality with authorization.
    Currently working on redditApiWrapper using authorization, authorization
    not working as intended.

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         'qYFWKLNrdeQafwiDVyUsjmddR',
+    consumer_secret:      'iJC1sgTsr5KTdzZwORQESeyErfwDKwCyOU07fDLZUUMxzUpaWu',
+    access_token:         '886693499411640320-rszZp6ssdFCQ2LUa3wmon5SrJXns2EU',
+    access_token_secret:  'QMglxi5ibWzaVq01bK0m4Jrnql9FqZtesaVoxMeY4nVqc'
+}
---
+++ redditConfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+    app_id: 'oot0dyzuCLqZQw',
+    app_secret: 'heJJOPBL36eDOtpgwhOXoaKJPoM',
+    redirect_uri: 'https://theseanathan.github.io/'
+}
