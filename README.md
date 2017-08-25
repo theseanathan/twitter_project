c9519cab8c9bc99eb2ed0be211c2363c6ef7dc2e
theseanathan <seandarryanto@gmail.com>
2017-08-25 14:10:39 -0400

    Full-cycle executable script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # twitter_project
-Not entirely sure what to do with it, but will likely use the Twit API OR Twitter4J.
+Gets the top posts on Reddit's r/ProgrammerHumor subreddit 
+and tweets the title and image of the top post. Logs previously
+tweeted post in a log 'reddit_post_log' so that it doesn't tweet
+duplicate posts. 
+
+Uses reddit-oauth for Reddit API authorization.
+
+Uses snoowrap for Reddit API wrapper.
+
+Uses Twit for Twitter API wrapper.
+
+Uses FileSystem to read/write logs.
+
+Uses Request to get base-64 encoding for the Reddit image to
+be submitted to Twitter.
--- reddit-twit.js
+++ reddit-twit.js
@@ -1,68 +1,96 @@
+const fs = require('fs');
+const snoowrap = require('snoowrap');
+var CONFIG = require('./config');
 var RedditApi = require('reddit-oauth');
-var request = require('request');
+var REDDITCONFIG = require('./redditConfig');
+var request = require('request').defaults({ encoding: null });
 var Snoowrap = require('snoowrap');
 var Twit = require('twit');
-var request = require('request').defaults({ encoding: null });
 
-var CONFIG = require('./config');
-var REDDITCONFIG = require('./redditConfig');
-
-var T = new Twit(CONFIG);
 var reddit = new RedditApi(REDDITCONFIG);
-const snoowrap = require('snoowrap');
-const fs = require('fs');
+var T = new Twit(CONFIG);
 
+const POST_LOG_PATH = "reddit_post_logs";
+const SUBREDDIT_TO_VIEW = 'ProgrammerHumor';
+const MINUTES_BETWEEN_TWEETS = 20;
 var access_token = '';
 var app_id = 'oot0dyzuCLqZQw';
 var app_secret = 'heJJOPBL36eDOtpgwhOXoaKJPoM';
-var redirect_uri = 'https://theseanathan.github.io/';
-var redditUsername = 'theseanathan';
 var redditPassword = 'sd1530';
+var redditUsername = 'theseanathan';
+var redirect_uri = 'https://theseanathan.github.io/';
 
-reddit.passAuth('theseanathan', 'sd1530', function (success) {
-    if (success) {
-        access_token = reddit.access_token;
+var secondsBetweenTweets = MINUTES_BETWEEN_TWEETS * 60;
+var millisecondsBetweenTweets = secondsBetweenTweets * 1000;
 
-        var redSno = new snoowrap({
-            userAgent: 'theseanathan',
-            clientId: app_id,
-            clientSecret: app_secret,
-            username: redditUsername,
-            password: redditPassword,
-            accessToken:  access_token
-        });
+console.log("Starting...");
 
-        //console.log("REDSNO OBJECT CREATED: " + JSON.stringify(redSno, null, 2));
+setInterval(function() { tweetHotPost() }, millisecondsBetweenTweets);
 
-        console.log("\n\nPROGRAMMER HUMOR TOP:");
-        redSno.getSubreddit('ProgrammerHumor').getTop().then(function(submissions) {
-            var topPostTitle = submissions[0].title;
-            var topPostThumbnail = submissions[0].thumbnail;
+function tweetHotPost() {
+    console.log("Starting process...");
+    reddit.passAuth('theseanathan', 'sd1530', function (success) {
+        if (success) {
+            console.log("Authorized Reddit API...");
+            access_token = reddit.access_token;
 
-            //console.log(JSON.stringify(submissions, null, 2));
-
-            var imageSource = submissions[0].preview.images[0].source.url;
-            //console.log(imageSource);
-            var imageInB64;
-
-            request.get(imageSource, function (error, response, body) {
-                if (!error && response.statusCode == 200) {
-                    data = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
-                    imageInB64 = data;
-                    tweetRedditPost(imageInB64);
-                }
-                console.log("OUT OF IF STATTEMENT");
+            var redSno = new snoowrap({
+                userAgent: 'theseanathan',
+                clientId: app_id,
+                clientSecret: app_secret,
+                username: redditUsername,
+                password: redditPassword,
+                accessToken:  access_token
             });
-        });
-    }
-});
 
-function tweetRedditPost(topPostImg) {
+            redSno.getSubreddit(SUBREDDIT_TO_VIEW).getHot().then(function(submissions) {
+                console.log("Getting subreddit...");
+                var topPostTitle;
+                var imageSource;
+                var topPostAuthor;
+                var topPostId;
+                var pastPosts;
+          
+                fs.readFile(POST_LOG_PATH, function(err, success) {
+                    console.log("Looking for duplicate posts...");
+                    pastPosts = "" + success;
+                    pastPosts = pastPosts.split('\n');
+
+                    for (var submission of submissions) {
+                        if (!pastPosts.includes(submission.id)) {
+                            topPostTitle = submission.title;
+                            imageSource = submission.url;
+                            topPostId = submission.id;
+                            break;
+                        }
+                    };
+                    
+                    logPost(topPostId);
+                    postImageUrlAsTweet(imageSource, topPostTitle);
+                });
+            });
+        }
+    });
+};
+
+function tweetRedditPost(topPostTitle, topPostImg) {
+    console.log("Tweeting post...");
     T.post('media/upload', { media_data: topPostImg }, function (err, data, response) {
-        console.log("TWEETED B64 IMAGE URL!");
-        console.log("DATA: " + JSON.stringify(data));
-        console.log("RESPONSE: " + JSON.stringify(response));
-    })
+        var mediaIdStr = data.media_id_string;
+        var altText = "This was supposed to be a picture but iss not smh";
+
+        var meta_params = { media_id: mediaIdStr, alt_text: { text: altText  }  }
+
+        T.post('media/metadata/create', meta_params, function (err, data, response) {
+            if (!err) {
+                var params = { status: topPostTitle, media_ids: [mediaIdStr]  }
+
+                T.post('statuses/update', params, function (err, data, response) {
+                    console.log("Tweeted successfully! " + data.created_at);
+                });
+            }
+        });
+    });
 };
 
 function getBase64Image(img) {
@@ -75,16 +103,42 @@ function getBase64Image(img) {
     return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
 }
 
-function getSDarryantoTweet() {
+function getUserTimeline(username) {
     var param = {
-        user_id: 'sdarryanto',
-        count: '1',
+        screen_name: username,
+        count: '200',
         include_rts: 'false'
-    }
+    };
 
     T.get('statuses/user_timeline', param, function(err, data, response) {
-        console.log(data[0].text);
+        data.forEach(function(tweet) {
+            console.log(tweet.text + "\n");
+        });
         process.exit();
     });
+
 };
+
+function logPost(postId) {
+    function callback(err, data) {
+        if (!err) {
+            console.log("Logged post successfully...");
+        } else {
+            console.log("Error logging post!!");
+        }
+    };
+
+    fs.appendFile(POST_LOG_PATH, postId + '\n', callback);
+};
+
+function postImageUrlAsTweet(imageSource, topPostTitle) {
+    var imageInB64;
+    request.get(imageSource, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            data = new Buffer(body).toString('base64');
+            imageInB64 = data;
+            tweetRedditPost(topPostTitle, imageInB64);
+        }
+    });
+}
 
